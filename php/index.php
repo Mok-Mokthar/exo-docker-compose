c88d9de1af2566523b1c234a7dd0c26e501d94f0
Mok-Mokthar <antoinechambrin@gmail.com>
2023-01-02 11:54:44 +0100

    initial commit

---
+++ php/index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Hello Man</title>
+    </head>
+
+    <body>
+        <?php
+            echo "Jure ça marche";
+            phpinfo(INFO_GENERAL);
+        ?>
+    </body>
+</html>
